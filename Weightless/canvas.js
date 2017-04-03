a8e4ccfa4400f6a5da15c4e61b986f4d5d12b2ee
Fortiflex <fortiflex@gmail.com>
2017-04-03 09:56:11 +0300

    add succes block

---
+++ Weightless/canvas.js
@@ -0,0 +1,18 @@
+//function makeFoto() {
+//    var article = document.querySelectorAll('.succes_history');
+//    for (var i = 0; i < article.length; i++) {
+//        var canvas = article[i].querySelector('.succes_img');
+//        var img1 = new Image();
+//        img1.onload = function() {
+//            c.drawImage(img1, 10, 10);
+//        };
+//        var img1Src = article[i].querySelector('.succes_first_img').src;
+//        var x = img1Src.split('/');
+//        img1.src = x[x.length-2] + '/' + x[x.length-1];
+//        console.log(img1);
+//          var c = canvas.getContext('2d');
+//        
+//        console.log('privet2');
+//    }
+//}
+//makeFoto();
