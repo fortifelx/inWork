32edd41d9558e0165ac4f22ba8afcc6955a3ff53
Fortiflex <fortiflex@gmail.com>
2017-04-19 08:04:07 +0300

    add slider script

--- IS/script.js
+++ IS/script.js
@@ -19,5 +19,47 @@
 
 })();
 
+(function(){
+    function makeSlider(sliderViewer,sliderName, slide, scrollTime, visibleElements, rightArrow, leftArrow ) {
+        var viewer = document.querySelector(sliderViewer);
+        var slider = document.querySelector(sliderName);
+        var slides = slider.querySelector(slide);
+        var lt = viewer.querySelector(rightArrow);
+        var gt = viewer.querySelector(rightArrow);
+        var step = slides[0].offsetWidth;
+        var status = 0;
+        var counter = visibleElements;
+        var correction = visibleElements;
+        
+        function scrollLeft() {
+            var edge = slides.length*step - correction*step;
+            if (counter <= correction) {
+                counter = slides.length;
+                Velocity( slider, {"margin-left": -edge}, scrollTime*3);
+                status = -edge;
+                return;
+            };
+            Velocity( slider, {'margin-left': status + step}, scrollTime);
+            counter--;
+            status = status + step;
+        };
+        
+        function scrollRight() {
+            var edge = slides.length*step;
+            if (counter >= slides.length) {
+                counter = correction;
+                Velocity( slider, {"margin-left": 0}, scrollTime*3);
+                status = 0;
+                return;
+            };
+            Velocity( slider, {'margin-left': status - step}, scrollTime);
+            counter++;
+            status = status - step;
+        };
+        
+    lt.addEventListener('click', scrollLeft);
+    gt.addEventListener('click', scrollRight);
     
+    };
+})();    
     
