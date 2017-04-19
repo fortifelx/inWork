87b5ee4b6b65a1f0f82fdfa6a0180fdee2c72129
Fortiflex <fortiflex@gmail.com>
2017-04-19 09:23:00 +0300

    who_we slider done!

--- IS/script.js
+++ IS/script.js
@@ -23,8 +23,83 @@
     function makeSlider(sliderViewer,sliderName, slide, scrollTime, visibleElements, rightArrow, leftArrow ) {
         var viewer = document.querySelector(sliderViewer);
         var slider = document.querySelector(sliderName);
-        var slides = slider.querySelector(slide);
-        var lt = viewer.querySelector(rightArrow);
+        var slides = slider.querySelectorAll(slide);
+        var lt = document.querySelector(leftArrow);
+        var gt = document.querySelector(rightArrow);
+        var step = slides[0].offsetWidth;
+        var status = 0;
+        var counter = visibleElements;
+        var correction = visibleElements;
+        var number =document.querySelector('.slide_number b');
+        
+        function scrollLeft() {
+            var edge = slides.length*step - correction*step;
+            if (counter <= correction) {
+                counter = slides.length;
+                Velocity( slider, {"margin-left": -edge}, scrollTime);
+                status = -edge;
+                number.innerHTML = '0' + counter;
+                Velocity( gt, { rotateZ: '0deg'}, 600);
+                return;
+            };
+            Velocity( slider, {'margin-left': status + step}, scrollTime);
+            counter--;
+            status = status + step;
+            Velocity( gt, { rotateZ: '-180deg'}, 600);
+        };
+        
+        function scrollRight() {
+            var edge = slides.length*step;
+            if (counter >= slides.length) {
+                counter = correction;
+                Velocity( slider, {"margin-left": 0}, scrollTime);
+                status = 0;
+                number.innerHTML = '0' + counter;
+                Velocity( gt, { rotateZ: '0deg'}, 600);
+                return;
+            };
+            Velocity( slider, {'margin-left': status - step}, scrollTime);
+            counter++;
+            status = status - step;
+            number.innerHTML = '0' + counter;
+            Velocity( gt, { rotateZ: '-180deg'}, 600);
+        };
+        
+   // lt.addEventListener('click', scrollLeft);
+    gt.addEventListener('click', scrollRight);
+    
+    };
+    
+    function textSlider(textBlock, rightArrow ) {
+        var gt = document.querySelector(rightArrow);
+        var text = document.querySelectorAll(textBlock);
+        var status = 0;
+        
+        function changeText() {
+            status++;
+            for(i = 0; i < text.length; i++) {
+                    Velocity( text[i], {opacity: '0'}, 300);
+            }
+            if(status >= text.length) {
+                        status = 0;
+               
+            }
+            Velocity( text[status], {opacity: '1'}, 300);
+        }
+        gt.addEventListener('click', changeText);
+    };
+    
+    makeSlider('.slide_viewer', '.slider_wrapper', 'li', 600, 1, '.slide_subtext img');
+    
+    textSlider('.slide_text', '.slide_subtext img');
+    
+})();  
+(function(){
+    function makeSlider(sliderViewer,sliderName, slide, scrollTime, visibleElements, rightArrow, leftArrow ) {
+        var viewer = document.querySelector(sliderViewer);
+        var slider = document.querySelector(sliderName);
+        var slides = slider.querySelectorAll(slide);
+        var lt = viewer.querySelector(leftArrow);
         var gt = viewer.querySelector(rightArrow);
         var step = slides[0].offsetWidth;
         var status = 0;
@@ -61,5 +136,5 @@
     gt.addEventListener('click', scrollRight);
     
     };
-})();    
+})();  
     
