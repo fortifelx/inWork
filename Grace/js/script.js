8a608b587d7dad4e76bb265b694115df9b3d93f9
Fortiflex <fortiflex@gmail.com>
2017-10-04 10:25:05 +0300

    add some fix

--- Grace/js/script.js
+++ Grace/js/script.js
@@ -264,122 +264,6 @@ scrollTop: top
     $rArrow.click(makeSlider);
     // setInterval(makeSlider, 10000);
 })();
-//(function(){
-//    var $wrapper    = $(".result-slider-wrapper");
-//    if($wrapper.length === 0) return;
-//    var $slides     = $(".result-slide");
-//    var slidesWidth = $slides.css('width');
-//    var $lArrow = $(".results-l-arrow");
-//    var $rArrow = $(".results-r-arrow");
-//    var slidesLength = $slides.length;
-//    var slidewidth  = (parseInt(slidesWidth, 10));
-//    $slides.css("left" , slidesWidth);
-//    $($slides[0]).css("left", 0);
-//    var counter = 0;
-//    var currentElement = $slides[0];
-//    var prevElement =  $slides[0];
-//    var sliderStatus = true;
-//    var $countText = $(".results-counter-text");
-//    var counterText = counter+1+"/"+slidesLength;
-//    $countText.text(counterText);
-//    function makeSlider(event){
-//        if(sliderStatus === false) return;
-//        sliderStatus = false;
-//        var el;
-//        if(el === undefined) {
-//            el = counter+1;
-//        };
-//        if(event) {
-//        	if(event.target === $lArrow[0]) {
-//        		el=counter-1;
-//        		if(el === -1) {
-//        			el = slidesLength - 1;
-//        	var $slide = $($slides[el]);
-//            $slide.css("left", "-" + slidesWidth);
-//            $slide.velocity({
-//                left : 0
-//            }, 1200);
-//            $(currentElement).velocity({
-//                left : slidesWidth
-//            }, 1200);
-//            counter = slidesLength - 1;    
-//            currentElement = $slides[el];
-//            setTimeout(function(){
-//                sliderStatus = true;
-//            }, 1200);
-//            counterText = counter+1+"/"+slidesLength;
-//            $countText.text(counterText);
-//            return;
-//        		}
-//        	};
-//        	if(event.target === $rArrow[0]) {
-//        		el=counter+1;
-//        		if(el > slidesLength - 1) {
-//        			el = 0;
-//            var $slide = $($slides[el]);
-//            $slide.css("left", slidesWidth);
-//            var slideLeft = "-" + slidesWidth;
-//            $slide.velocity({
-//                left : 0
-//            }, 1200);
-//            $(currentElement).velocity({
-//                left : slideLeft
-//            }, 1200);
-//            counter = el;    
-//            currentElement = $slides[el];
-//            setTimeout(function(){
-//                sliderStatus = true;
-//            }, 1200);
-//            counterText = counter+1+"/"+slidesLength;
-//            $countText.text(counterText);
-//            return;
-//        		}
-//        	};
-//            }
-//        if (el < counter) {
-//            var $slide = $($slides[el]);
-//            $slide.css("left", "-" + slidesWidth);
-//            $slide.velocity({
-//                left : 0
-//            }, 1200);
-//            $(currentElement).velocity({
-//                left : slidesWidth
-//            }, 1200);
-//            counter = el;    
-//            currentElement = $slides[el];
-//            setTimeout(function(){
-//                sliderStatus = true;
-//            }, 1200);
-//            counterText = counter+1+"/"+slidesLength;
-//            $countText.text(counterText);
-//            return;
-//        }
-//         if (el > counter) {
-//            var $slide = $($slides[el]);
-//            $slide.css("left", slidesWidth);
-//            var slideLeft = "-" + slidesWidth;
-//            $slide.velocity({
-//                left : 0
-//            }, 1200);
-//            $(currentElement).velocity({
-//                left : slideLeft
-//            }, 1200);
-//            counter = el;    
-//            currentElement = $slides[el];
-//            setTimeout(function(){
-//                sliderStatus = true;
-//            }, 1200);
-//            counterText = counter+1+"/"+slidesLength;
-//            $countText.text(counterText);
-//            return;
-//        } else {
-//            return;
-//        }
-//    };
-//    $lArrow.click(makeSlider);
-//    $rArrow.click(makeSlider);
-//    // setInterval(makeSlider, 10000);
-//})();
 (function(){
     var $wrapper = $('.result-slider-wrapper');
     var $viewerWidth = (parseInt($(".result-slider").css("width"), 10));
@@ -393,7 +277,7 @@ scrollTop: top
     var slidesWidth = (parseInt($slides.css("width"), 10));
     var wrapperWidth = slidesLength*slidesWidth + "px";
     $(window).resize(function(){
-        $viewerWidth = (parseInt($(".main-brand-viewer").css("width"), 10));
+        $viewerWidth = (parseInt($(".result-slider").css("width"), 10));
         $wrapper.css("margin-left", 0);
         position = 0;
         counter = 0;
