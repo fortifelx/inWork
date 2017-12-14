1b07af3bde72f3e7d3886e6d3303aa25f9d068b5
Fortifelx <fortifelx@gmail.com>
2017-12-14 11:02:34 +0200

    add fix

--- Grace/js/script.js
+++ Grace/js/script.js
@@ -7,6 +7,14 @@ $('body,html').animate({
 scrollTop: top
 }, 1000);
 });
+$(".menu-connect-btn").click(function(event){
+ event.preventDefault();
+var id = $(this).attr('href')
+, top = $(id).offset().top;
+$('body,html').animate({
+scrollTop: top
+}, 1000);
+})
 });
 (function(){
     var $wrapper    = $(".about-slider-wrapper");
