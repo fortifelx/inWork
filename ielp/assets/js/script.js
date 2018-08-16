2536ed67d20ff7b8dd9b949133ac053d277aa3ac
Fortifelx <fortifelx@gmail.com>
2018-08-16 11:48:36 +0300

    add last scroll fix

--- ielp/assets/js/script.js
+++ ielp/assets/js/script.js
@@ -1,21 +1,9 @@
-var scrollPosition = 0;
 var body = document.body,
     html = document.documentElement;
 
 var caseSlides = $('.case_slide');
 var slideHeight = caseSlides.css('height');
 var menuStatus = false;
-// left: 37, up: 38, right: 39, down: 40,
-// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
-makeScroll('.scroll_element', 1200);
-var x = 0;
-
-// var oldPos = $(window).scrollTop();
-var oldPos = $(window).scrollTop();
-var scrollStatus = false;
-// if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
-// window.onmousewheel = document.onmousewheel = wheel;
 var navCitiesStatus = true;
 
 
@@ -138,78 +126,7 @@ function makeMainSlider(slides, navImage, navItems, images, imagesViewer, texts,
 
 
 };
-function makeScroll(elements, time) {
 
-    var nl = $(elements);
-    var navSlide = $('.nav_slide li');
-    // var navMenu = $('.nav_wrapper');
-    var positions = [];
-    for (var i = nl.length; i--; positions.unshift($(nl[i]).offset().top)) ;
-    var i = 0;
-    var body = $("html, body");
-    var status = true;
-    //  body.animate({
-    //     scrollTop: 0,
-    // }, time);
-    navSlide.click(function(){
-        status = false;
-        $("html, body").animate({
-            scrollTop: 0,
-        }, 600);
-        setTimeout(function () {
-            status = true;
-            previousScrollPosition = 0;
-        }, time);
-        i = 0;
-        console.log('click');
-    });
-
-    var previousScrollPosition = $(window).scrollTop() + $(window).height();
-    $(window).scroll(function (e) {
-
-        var currentScrollPosition = $(window).scrollTop() + $(window).height();
-        if
-        (currentScrollPosition > previousScrollPosition + 1 && status)
-        {
-            if (i === positions.length - 1) return;
-            status = false;
-            i++;
-            // disableScroll();
-            // if (i === 0) {
-            //     showSlide(mainSlide, navMenu, $(window).height(), time*2);
-            // }
-            // if (i > 0) {
-            //     hideSlide(mainSlide, navMenu, $(window).height(), time / 2);
-            // }
-            body.stop().animate({
-                scrollTop: positions[i],
-            }, time);
-            setTimeout(function () {
-                status = true;
-            }, time);
-
-        } else
-            if(currentScrollPosition < previousScrollPosition && status)
-            {
-            if (i === 0) {
-                // showSlide(mainSlide, navMenu, $(window).height(), time*2);
-                return;
-            }
-            status = false;
-            i--;
-            // if (i === 0) {
-            //     showSlide(mainSlide, navMenu, $(window).height(), time*2);
-            // }
-            body.stop().animate({
-                scrollTop: positions[i],
-            }, time);
-            setTimeout(function () {
-                status = true;
-            }, time);
-        }
-        previousScrollPosition = currentScrollPosition;
-    });
-};
 function hideSlide(target, menu, size, time) {
     target.velocity({
         backgroundColor : 'rgba(33, 33, 33, 1)'
@@ -324,34 +241,6 @@ function longSlider(slide, viewerBlock, lArrow, rArrow, countBlock, dots, step, 
         }, time);
     });
 };
-function preventDefault(e) {
-    e = e || window.event;
-    if (e.preventDefault)
-        e.preventDefault();
-    e.returnValue = false;
-}
-function preventDefaultForScrollKeys(e) {
-    if (keys[e.keyCode]) {
-        preventDefault(e);
-        return false;
-    }
-}
-function disableScroll() {
-    if (window.addEventListener) // older FF
-        window.addEventListener('DOMMouseScroll', preventDefault, false);
-    window.onwheel = preventDefault; // modern standard
-    window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-    window.ontouchmove  = preventDefault; // mobile
-    document.onkeydown  = preventDefaultForScrollKeys;
-}
-function enableScroll() {
-    if (window.removeEventListener)
-        window.removeEventListener('DOMMouseScroll', preventDefault, false);
-    window.onmousewheel = document.onmousewheel = null;
-    window.onwheel = null;
-    window.ontouchmove = null;
-    document.onkeydown = null;
-}
 function showMenu(target, button, time){
     $(target).toggleClass('nav_cities_wrapper_active');
     $(button).toggleClass('nav_menu_button_active');
@@ -382,159 +271,9 @@ function showMenu(target, button, time){
         menuStatus = false;
     }
 }
-function wheel(event) {
-    var delta = 0;
-    if (event.wheelDelta) delta = event.wheelDelta / 120;
-    else if (event.detail) delta = -event.detail / 3;
-
-    handle(delta);
-    if (event.preventDefault) event.preventDefault();
-    event.returnValue = false;
-}
-function handle(delta) {
-    if(scrollStatus) return;
-    scrollStatus = true;
-    var time = 1000;
-    var positions = [];
-    var windowHeight = $(window).height();
-    var scrollPos = $(window).scrollTop();
-    var nl = $('.scroll_element');
-    // console.log(scrollPos);
-    var distance;
-    // console.log('new ' + scrollPos + '/' + 'old ' + oldPos);
-    for (var i = nl.length; i--; positions.unshift($(nl[i]).offset().top)) ;
-    // if(scrollPos < oldPos) {
-    //     console.log('up');
-    //     x--;
-    //     distance = scrollPos - positions[x] + windowHeight;
-    // }else
-    //     if(scrollPos > oldPos) {
-    console.log('down');
-    distance = scrollPos - positions[x] - windowHeight;
-    oldPos = scrollPos - distance;
-    console.log('new ' + distance + '/' + 'old ' + oldPos);
-    if(oldPos < distance) x--;
-    if(oldPos === distance) x++;
-    // if(distance < oldPos) x++;
-    console.log(x);
-    // if(oldPos){
-    //
-    // };
-    // oldPos = scrollPos - distance;
-    // }else if(scrollPos === oldPos){
-    //     console.log('down');
-    //     x++;
-    //     distance = scrollPos - positions[x-1] + windowHeight;
-    // };
-    // console.log(scrollPos - (distance * delta));
-    // distance = scrollPos - positions[x-1] + $(window).height();
-    $('html, body').stop().animate({
-        scrollTop: scrollPos - (distance * delta)
-    }, time );
-    // console.log('new ' + scrollPos + '/' + 'old ' + oldPos);
-    setTimeout(function(){
-        scrollStatus = false;
-    }, time);
-}
 function changeCitiesSize(cities){
     result = ($(window).height() - 100)/$(cities).length/2;
     $(cities).css({
         fontSize : result
     })
 }
-(function($) {
-
-    "use strict";
-
-    var instance, proto;
-
-    function UserScrollDisabler($container, options) {
-        // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-        // left: 37, up: 38, right: 39, down: 40
-        this.opts = $.extend({
-            handleKeys : true,
-            scrollEventKeys : [32, 33, 34, 35, 36, 37, 38, 39, 40]
-        }, options);
-
-        this.$container = $container;
-        this.$document = $(document);
-        this.lockToScrollPos = [0, 0];
-
-        this.disable();
-    }
-
-    proto = UserScrollDisabler.prototype;
-
-    proto.disable = function() {
-        var t = this;
-
-        t.lockToScrollPos = [
-            t.$container.scrollLeft(),
-            t.$container.scrollTop()
-        ];
-
-        t.$container.on(
-            "mousewheel.disablescroll DOMMouseScroll.disablescroll touchmove.disablescroll",
-            t._handleWheel
-        );
-
-        t.$container.on("scroll.disablescroll", function() {
-            t._handleScrollbar.call(t);
-        });
-
-        if(t.opts.handleKeys) {
-            t.$document.on("keydown.disablescroll", function(event) {
-                t._handleKeydown.call(t, event);
-            });
-        }
-    };
-
-    proto.undo = function() {
-        var t = this;
-        t.$container.off(".disablescroll");
-        if(t.opts.handleKeys) {
-            t.$document.off(".disablescroll");
-        }
-    };
-
-    proto._handleWheel = function(event) {
-        event.preventDefault();
-    };
-
-    proto._handleScrollbar = function() {
-        this.$container.scrollLeft(this.lockToScrollPos[0]);
-        this.$container.scrollTop(this.lockToScrollPos[1]);
-    };
-
-    proto._handleKeydown = function(event) {
-        for (var i = 0; i < this.opts.scrollEventKeys.length; i++) {
-            if (event.keyCode === this.opts.scrollEventKeys[i]) {
-                event.preventDefault();
-                return;
-            }
-        }
-    };
-
-
-    // Plugin wrapper for object
-    $.fn.disablescroll = function(method) {
-
-        // If calling for the first time, instantiate the object and save
-        // reference. The plugin can therefore only be instantiated once per
-        // page. You can pass options object in through the method parameter.
-        if( ! instance && (typeof method === "object" || ! method)) {
-            instance = new UserScrollDisabler(this, method);
-        }
-
-        // Instance already created, and a method is being explicitly called,
-        // e.g. .disablescroll('undo');
-        else if(instance && instance[method]) {
-            instance[method].call(instance);
-        }
-
-    };
-
-    // Global access
-    window.UserScrollDisabler = UserScrollDisabler;
-
-})(jQuery);
---
+++ ielp/assets/js/scroll.js
@@ -0,0 +1,209 @@
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+// var oldPos = $(window).scrollTop();
+var scrollPosition = 0;
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+var x = 0;
+var oldPos = $(window).scrollTop();
+var scrollStatus = false;
+makeScroll('.scroll_element', 1200);
+var previousScrollPosition = $(window).scrollTop() + $(window).height();
+var positions = [];
+var status = false;
+var i = 0;
+var time = 1200;
+
+
+function makeScroll(elements, time) {
+
+    var nl = $(elements);
+    var navSlide = $('.nav_slide li');
+    // var navMenu = $('.nav_wrapper');
+    var positions = [];
+    for (var i = nl.length; i--; positions.unshift($(nl[i]).offset().top)) ;
+    var i = 0;
+    var body = $("html, body");
+    var status = true;
+    //  body.animate({
+    //     scrollTop: 0,
+    // }, time);
+    navSlide.click(function(){
+        status = false;
+        $("html, body").animate({
+            scrollTop: 0,
+        }, 600);
+        setTimeout(function () {
+            status = true;
+            previousScrollPosition = 0;
+        }, time);
+        i = 0;
+        console.log('click');
+    });
+
+    var previousScrollPosition = $(window).scrollTop() + $(window).height();
+    $(window).scroll(function (e) {
+        preventDefault(e);
+        var currentScrollPosition = $(window).scrollTop() + $(window).height();
+        if
+        (currentScrollPosition > previousScrollPosition + 1 && status)
+        {
+            if (i === positions.length - 1) return;
+            status = false;
+            i++;
+            // disableScroll();
+            // if (i === 0) {
+            //     showSlide(mainSlide, navMenu, $(window).height(), time*2);
+            // }
+            // if (i > 0) {
+            //     hideSlide(mainSlide, navMenu, $(window).height(), time / 2);
+            // }
+            body.stop().animate({
+                scrollTop: positions[i],
+            }, time);
+            setTimeout(function () {
+                status = true;
+            }, time);
+
+        } else
+        if(currentScrollPosition < previousScrollPosition && status)
+        {
+            if (i === 0) {
+                // showSlide(mainSlide, navMenu, $(window).height(), time*2);
+                return;
+            }
+            status = false;
+            i--;
+            // if (i === 0) {
+            //     showSlide(mainSlide, navMenu, $(window).height(), time*2);
+            // }
+            body.stop().animate({
+                scrollTop: positions[i],
+            }, time);
+            setTimeout(function () {
+                status = true;
+            }, time);
+        }
+        previousScrollPosition = currentScrollPosition;
+    });
+};
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;
+}
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+function disableScroll() {
+    if (window.addEventListener) // older FF
+        window.addEventListener('DOMMouseScroll', preventDefault, false);
+    window.onwheel = preventDefault; // modern standard
+    window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+    window.ontouchmove  = preventDefault; // mobile
+    document.onkeydown  = preventDefaultForScrollKeys;
+}
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+}
+function wheel(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 120;
+    else if (event.detail) delta = -event.detail / 3;
+
+    handle(delta);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+function handle(delta) {
+    if(scrollStatus) return;
+    scrollStatus = true;
+    var time = 1000;
+    var positions = [];
+    var windowHeight = $(window).height();
+    var scrollPos = $(window).scrollTop();
+    var nl = $('.scroll_element');
+    var distance;
+    for (var i = nl.length; i--; positions.unshift($(nl[i]).offset().top)) ;
+    console.log('down');
+    distance = scrollPos - positions[x] - windowHeight;
+    oldPos = scrollPos - distance;
+    console.log('new ' + distance + '/' + 'old ' + oldPos);
+    if(oldPos < distance) x--;
+    if(oldPos === distance) x++;
+    console.log(x);
+    $('html, body').stop().animate({
+        scrollTop: scrollPos - (distance * delta)
+    }, time );
+    setTimeout(function(){
+        scrollStatus = false;
+    }, time);
+}
+function smoothScroll(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 120;
+    else if (event.detail) delta = -event.detail / 3;
+    var nl = $('.scroll_element');
+    var navSlide = $('.nav_slide li');
+    for (var y = nl.length; y--; positions.unshift($(nl[y]).offset().top)) ;
+    var body = $("html, body");
+    navSlide.click(function(){
+        status = false;
+        $("html, body").animate({
+            scrollTop: 0,
+        }, 600);
+        setTimeout(function () {
+            status = true;
+            previousScrollPosition = 0;
+        }, time);
+        i = 0;
+        console.log('click');
+    });
+
+    previousScrollPosition = $(window).scrollTop();
+
+
+    // alternativeScroll(body);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+function alternativeScroll(body){
+    var currentScrollPosition = $(window).scrollTop() + $(window).height();
+    console.log(status + '/' + i);
+        if(status){
+            console.log(currentScrollPosition + '/' + previousScrollPosition + '/' + status);
+            if (i === positions.length - 1) return;
+            status = false;
+            i++;
+            console.log('down');
+            body.animate({
+                scrollTop: positions[i],
+            }, time);
+            setTimeout(function () {
+                status = true;
+            }, time);
+
+        } else if(currentScrollPosition < previousScrollPosition && status){
+            if (i === 0) {
+                return;
+            }
+            console.log('up');
+            status = false;
+            i--;
+            body.animate({
+                scrollTop: positions[i],
+            }, time);
+            setTimeout(function () {
+                status = true;
+            }, time);
+        }
+        previousScrollPosition = currentScrollPosition;
+}
