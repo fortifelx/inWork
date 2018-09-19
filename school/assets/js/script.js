908e161c20b9f6339ec08a82b90208dde5c60457
Fortifelx <fortifelx@gmail.com>
2018-09-19 13:02:41 +0300

    add hover course

--- school/assets/js/script.js
+++ school/assets/js/script.js
@@ -8,13 +8,13 @@ $('.show_call img').click(function(){
 })
 })();
 (function(){
+    var count = document.querySelector('.advantage_count');
+    if(count == undefined) return;
     var length = $('.advantage_slide').length;
     var i = 0;
-    var count = document.querySelector('.advantage_count');
-    console.log(count);
+
     count.innerHTML = '<span>1/</span>' + length;
    $('.advantage_right_arrow').click(function(){
-      console.log(i);
       i++;
       if(i === length) i = 0;
       $('.advantage_slide').removeClass('advantage_slide_active');
@@ -23,10 +23,16 @@ $('.show_call img').click(function(){
       count.innerHTML = '<span>' + x +'/</span>' + length;
    });
    $('.advantage_left_arrow').click(function(){
-      console.log(i);
       i--;
       if(i === -1) i = length - 1;
       $('.advantage_slide').removeClass('advantage_slide_active');
       $($('.advantage_slide')[i]).addClass('advantage_slide_active');
    });
+})();
+(function(){
+    console.log('here');
+    $('.start_block_header').click(function(){
+        console.log('here');
+        $(this).toggleClass('start_active_header').next().toggleClass('hover_course_wrapper');
+    })
 })();
